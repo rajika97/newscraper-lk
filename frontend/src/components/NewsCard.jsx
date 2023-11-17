eb5169c95645fc1d5fdb389456eefeae92af9f56
rajika97 <lprajika@gmail.com>
2023-11-17 11:01:45 +0530

    Change the design on News Card component

--- frontend/src/components/NewsCard.jsx
+++ frontend/src/components/NewsCard.jsx
@@ -1,61 +1,102 @@
-import React from "react";
+import * as React from "react";
+import AspectRatio from "@mui/joy/AspectRatio";
+import Card from "@mui/joy/Card";
+import CardContent from "@mui/joy/CardContent";
+import CardOverflow from "@mui/joy/CardOverflow";
+import Divider from "@mui/joy/Divider";
+import Typography from "@mui/joy/Typography";
+import { AiOutlineCamera } from "react-icons/ai";
 
 const NewsCard = ({ image, date, title, link }) => {
   // Function to format the date and time
-  const formatDate = (dateString) => {
-    const options = { month: "short", day: "numeric" };
-    const formattedDate = new Date(dateString).toLocaleDateString(
-      undefined,
-      options
-    );
 
-    // Adding 6 hours and 30 minutes to the time
-    const originalTime = new Date(dateString).getTime();
-    const adjustedTime = originalTime + 18 * 60 * 60 * 1000 + 30 * 60 * 1000;
-    const formattedTime = new Date(adjustedTime).toLocaleTimeString("en-US", {
-      hour: "2-digit",
-      minute: "2-digit",
-      hour12: true,
-    });
+  const formatDate = (dateString) => {
+    const dateTime = new Date(dateString);
+    const today = new Date();
+
+    // Check if the date is today
+    if (
+      dateTime.getUTCFullYear() === today.getUTCFullYear() &&
+      dateTime.getUTCMonth() === today.getUTCMonth() &&
+      dateTime.getUTCDate() === today.getUTCDate()
+    ) {
+      const hours = dateTime.getUTCHours();
+      const minutes = dateTime.getUTCMinutes();
+      const ampm = hours >= 12 ? "PM" : "AM";
+      const formattedTime = `${hours % 12 || 12}:${
+        minutes < 10 ? "0" + minutes : minutes
+      } ${ampm}`;
+      return { formattedDate: "Today", formattedTime };
+    }
+
+    // Extract date components
+    const monthAbbreviation = new Intl.DateTimeFormat("en-US", {
+      month: "short",
+    }).format(dateTime);
+    const day = dateTime.getUTCDate();
+
+    let hours = dateTime.getUTCHours();
+    const minutes = dateTime.getUTCMinutes();
+    const ampm = hours >= 12 ? "PM" : "AM";
+    hours = hours % 12 || 12; // Convert 0 to 12 for midnight
+
+    const formattedDate = `${monthAbbreviation} ${day}`;
+    const formattedTime = `${hours < 10 ? "0" + hours : hours}:${
+      minutes < 10 ? "0" + minutes : minutes
+    } ${ampm}`;
 
     return { formattedDate, formattedTime };
   };
 
   const { formattedDate, formattedTime } = formatDate(date);
 
+  const isImageLink = image.match(/\.(jpeg|jpg|png)$/);
+
   return (
-    <div className="flex-shrink-0 w-full md:w-1/2 lg:w-1/3 px-6 mb-8">
-      <div className="mb-8 overflow-hidden rounded">
-        <a href={link} target="_blank" rel="noopener noreferrer">
-          <img
-            src={image}
-            alt=""
-            className="w-full h-64 object-cover rounded-lg"
-          />
-        </a>
-      </div>
-      <div>
-        <h3>
-          <a
-            href={link}
-            className="mb-2 inline-block text-xl font-semibold text-dark hover:text-primary dark:text-black sm:text-2xl lg:text-xl xl:text-2xl"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            {title}
+    <div className="flex-shrink-0 w-full sm:w-1/2 md:w-1/2 lg:w-1/4 px-6 mb-8 ">
+      <Card variant="outlined" sx={{ width: 320 }}>
+        <CardOverflow>
+          <AspectRatio ratio="2">
+            {isImageLink ? (
+              <a href={link} target="_blank" rel="noopener noreferrer">
+                <img src={image} srcSet={image} loading="lazy" alt={title} />
+              </a>
+            ) : (
+              <a href={link} target="_blank" rel="noopener noreferrer">
+                <AiOutlineCamera
+                  size={200}
+                  className="w-full h-48 rounded-lg object-cover"
+                />
+              </a>
+            )}
+          </AspectRatio>
+        </CardOverflow>
+        <CardContent>
+          <a href={link} target="_blank" rel="noopener noreferrer">
+            <Typography level="title-lg">{title}</Typography>
           </a>
-        </h3>
-        {date && (
-          <div className="flex items-center">
-            <span className="mr-2 inline-block rounded bg-primary px-3 py-1 text-center text-sm font-semibold leading-loose text-black">
+        </CardContent>
+        <CardOverflow variant="soft" sx={{ bgcolor: "background.level1" }}>
+          <Divider inset="context" />
+          <CardContent orientation="horizontal">
+            <Typography
+              level="body-xs"
+              fontWeight="md"
+              textColor="text.secondary"
+            >
               {formattedDate}
-            </span>
-            <span className="inline-block rounded bg-primary px-3 py-1 text-center text-sm font-semibold leading-loose text-black">
+            </Typography>
+            <Divider orientation="vertical" />
+            <Typography
+              level="body-xs"
+              fontWeight="md"
+              textColor="text.secondary"
+            >
               {formattedTime}
-            </span>
-          </div>
-        )}
-      </div>
+            </Typography>
+          </CardContent>
+        </CardOverflow>
+      </Card>
     </div>
   );
 };
