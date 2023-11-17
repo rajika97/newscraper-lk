2e12363d05ce843fc81cda54e5e5864c7ed33fbb
rajika97 <lprajika@gmail.com>
2023-11-17 13:18:33 +0530

    Allow requestes from new URL

--- backend/index.js
+++ backend/index.js
@@ -13,7 +13,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: ["http://localhost:5173", "https://newscraper-lk.vercel.app"],
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type"],
   })
