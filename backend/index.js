ed6105454b7392502c53a8ae4ac151501e851cb3
rajika97 <lprajika@gmail.com>
2023-11-17 14:11:52 +0530

    Allow requests from all source

--- backend/index.js
+++ backend/index.js
@@ -13,7 +13,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: ["http://localhost:5173", "https://newscraper-lk.vercel.app"],
+    origin: ["*"],
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type"],
   })
