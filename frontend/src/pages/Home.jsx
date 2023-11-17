d84763d2a1dc813c8e8605284af9d352a07ef4e2
rajika97 <lprajika@gmail.com>
2023-11-17 11:00:53 +0530

    Add new News Sources to home page

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -8,6 +8,9 @@ const Home = () => {
   const [loading, setLoading] = useState(false);
   const [hiruNews, setHiruNews] = useState([]);
   const [deranaNews, setDeranaNews] = useState([]);
+  const [itnNews, setItnNews] = useState([]);
+  const [sirasaNews, setSirasaNews] = useState([]);
+  const [asianMirrorNews, setAsianMirrorNews] = useState([]);
   useEffect(() => {
     setLoading(true);
     axios
@@ -30,20 +33,67 @@ const Home = () => {
       .catch((err) => {
         console.log(err);
       });
+    axios
+      .get("http://localhost:3000/itn/1")
+      .then((res) => {
+        console.log(res.data);
+        setItnNews(res.data);
+        setLoading(false);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+    axios
+      .get("http://localhost:3000/sirasa/1")
+      .then((res) => {
+        console.log(res.data);
+        setSirasaNews(res.data);
+        setLoading(false);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+    axios
+      .get("http://localhost:3000/asianmirror/1")
+      .then((res) => {
+        console.log(res.data);
+        setAsianMirrorNews(res.data);
+        setLoading(false);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
   }, []);
   return (
-    <>
-      <h1 className="mx-8 text-xl">Hiru News</h1>
-      {loading && <Spinner />}
-      <div className="flex flex-wrap justify-around">
-        <CardList newsList={hiruNews} />
+    <div className="body">
+      <div className="main">
+        <h1 className="mx-8 text-xl">Hiru News</h1>
+        {loading && <Spinner />}
+        <div className="flex flex-wrap justify-around">
+          <CardList newsList={hiruNews} />
+        </div>
+        <h1 className="mx-8 text-xl">Derana News</h1>
+        {loading && <Spinner />}
+        <div className="flex flex-wrap justify-around">
+          <CardList newsList={deranaNews} />
+        </div>
+        <h1 className="mx-8 text-xl">Sirasa News</h1>
+        {loading && <Spinner />}
+        <div className="flex flex-wrap justify-around">
+          <CardList newsList={sirasaNews} />
+        </div>
+        <h1 className="mx-8 text-xl">ITN News</h1>
+        {loading && <Spinner />}
+        <div className="flex flex-wrap justify-around">
+          <CardList newsList={itnNews} />
+        </div>
+        <h1 className="mx-8 text-xl">Asian Mirror News</h1>
+        {loading && <Spinner />}
+        <div className="flex flex-wrap justify-around">
+          <CardList newsList={asianMirrorNews} />
+        </div>
       </div>
-      <h1 className="mx-8 text-xl">Derana News</h1>
-      {loading && <Spinner />}
-      <div className="flex flex-wrap justify-around">
-        <CardList newsList={deranaNews} />
-      </div>
-    </>
+    </div>
   );
 };
 
