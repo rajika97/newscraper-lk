406996e5d0f81fe42794f22278f3ce8033426c1e
rajika97 <lprajika@gmail.com>
2023-11-18 15:53:58 +0530

    Import puppeteer core

--- backend/events/scrapeDeranaNews.js
+++ backend/events/scrapeDeranaNews.js
@@ -1,20 +1,32 @@
-import puppeteer from "puppeteer";
+import puppeteerBrowser from "puppeteer";
+import puppeteerCore from "puppeteer-core";
+import chromeAwsLambda from "chrome-aws-lambda";
+
+let chrome = {};
+let puppeteer;
+
+if (process.env.AWS_LAMBDA_FUNCTION_VERSION) {
+  chrome = chromeAwsLambda;
+  puppeteer = puppeteerCore;
+} else {
+  puppeteer = puppeteerBrowser;
+}
 
 export const scrapeDeranaNews = async (req, res) => {
   const pageNumber = parseInt(req.params.page);
-  const browser = await puppeteer.launch({
-    headless: "new",
-    args: [
-      "--disable-setuid-sandbox",
-      "--no-sandbox",
-      "--single-process",
-      "--no-zygote",
-    ],
-    executablePath:
-      process.env.NODE_ENV === "production"
-        ? process.env.PUPPETEER_EXECUTABLE_PATH
-        : puppeteer.executablePath(),
-  });
+  let options = { headless: "new" };
+
+  if (process.env.AWS_LAMBDA_FUNCTION_VERSION) {
+    options = {
+      args: [...chrome.args, "--hide-scrollbars", "--disable-web-security"],
+      defaultViewport: chrome.defaultViewport,
+      executablePath: await chrome.executablePath,
+      headless: true,
+      ignoreHTTPSErrors: true,
+    };
+  }
+
+  const browser = await puppeteer.launch(options);
   try {
     const page = await browser.newPage();
     await page.goto(
