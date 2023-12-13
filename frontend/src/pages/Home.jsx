4eddc39ad59af402dcd316414e05d97239e3f143
rajika97 <lprajika@gmail.com>
2023-12-14 00:24:20 +0530

    Change the order of news sections

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -8,11 +8,11 @@ const Home = () => {
     <div className="body">
       <div className="main pl-8">
         <Header />
-        <NewsSection title="Derana News" route="derana" />
-        <NewsSection title="Live At 8 News" route="liveat8" />
-        <NewsSection title="ITN News" route="itn" />
-        <NewsSection title="Asian Mirror News" route="asianmirror" />
         <NewsSection title="Hiru News" route="hiru" />
+        <NewsSection title="ITN News" route="itn" />
+        <NewsSection title="Derana News" route="derana" />
+        <NewsSection title="Asian Mirror News" route="asianmirror" />
+        <NewsSection title="Live At 8 News" route="liveat8" />
       </div>
     </div>
   );
