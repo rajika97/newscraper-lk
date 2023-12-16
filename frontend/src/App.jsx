96d15d8a04d8c47fd3f692d58d41ffd87ed3d40e
rajika97 <lprajika@gmail.com>
2023-12-16 16:54:27 +0530

    Fix a bug

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,12 +2,10 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import NewsPage from "./pages/NewsPage";
-import GoogleAnalytics from "./components/GoogleAnalytics";
 
 const App = () => {
   return (
     <Routes>
-      <GoogleAnalytics />
       <Route path="/" element={<Home />} />
       <Route path="/:newsRoute" element={<NewsPage />} />
     </Routes>
