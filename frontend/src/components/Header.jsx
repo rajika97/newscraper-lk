0786f2ec99142f1bf866dd3eea3ef16109192684
rajika97 <lprajika@gmail.com>
2023-12-16 14:08:46 +0530

    Add a clock to the header

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,4 +1,32 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+
+const DigitalClock = () => {
+  const [currentTime, setCurrentTime] = useState(new Date());
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setCurrentTime(new Date());
+    }, 1000);
+
+    return () => clearInterval(intervalId);
+  }, []);
+
+  const hours = currentTime.getHours() % 12 || 12; // Convert to 12-hour format
+  const minutes = currentTime.getMinutes();
+  const seconds = currentTime.getSeconds();
+  const amPm = currentTime.getHours() >= 12 ? "PM" : "AM";
+
+  const formattedTime = `${hours}:${minutes < 10 ? "0" : ""}${minutes}:${
+    seconds < 10 ? "0" : ""
+  }${seconds}  ${amPm}`;
+
+  return (
+    <div className="rounded-3xl bg-white py-3 px-8 text-center">
+      <p className="text-lg">Real-Time Sri Lankan News</p>
+      <p className="text-2xl font-bold">{formattedTime}</p>
+    </div>
+  );
+};
 
 const Header = () => {
   return (
@@ -13,6 +41,9 @@ const Header = () => {
           don't have to. Get all the updates without the hassle of jumping
           between different sites
         </h3>
+      </div>
+      <div className="mt-4">
+        <DigitalClock />
       </div>
     </div>
   );
